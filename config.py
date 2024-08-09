26cf6a6f4afe75e073edda637117c67e86ab0eda
SYNAX-SANATANI <166273111+SYNAX-SANATANI@users.noreply.github.com>
2024-08-09 12:46:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,15 +34,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ________________________________________________________________________________#
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/istkharalam62/PURVI-MUSIC",
+    "https://github.com/SYNAX-SANATANI/PROFESSOR-MUSIC2",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 # ________________________________________________________________________________#
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/PURVI_UPDATES")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/purvi_support")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/PROFESSOR_NETWORK")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/PROFESSORXNETWORK")
 # ________________________________________________________________________________#
 # Set this to True if you want the assistant to automatically leave chats
 # after an interval
